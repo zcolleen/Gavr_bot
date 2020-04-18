ddea24a185ee1dbdb2aab1c40dde8aa5a95d4b08
zcolleen <semeneigin@yandex.ru>
2020-04-18 15:01:03 +0300

    2

--- bot/bot1.py
+++ bot/bot1.py
@@ -6,7 +6,7 @@ class Gavr:
 
     def __init__(self,username=''):
         
-        #отключаем все звуки в браузере на время нашей сессии
+        #отключаем все звуки в браузере на время нашей сессии и задаем стандартные размеры окна хрома
         chrome_option = webdriver.ChromeOptions()
         chrome_option.add_argument("--mute-audio")
         chrome_option.add_argument("--window-size=900,800")
@@ -22,19 +22,19 @@ class Gavr:
         
     def login(self):
 
-        self.driver.get('http://webinar.bmstu.ru/b/vpw-2zy-3k2')# это ссылка на мой баттон
+        self.driver.get('https://webinar.bmstu.ru/b/vpw-2zy-3k2')# это ссылка на мой баттон
         #http://webinar.bmstu.ru/b/q9y-xew-zr4 - это ссылка на Гаврюшина
         sleep(2)
 
-        code_access = self.driver.find_element_by_xpath('//*[@id="room_access_code"]')
+        code_access = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div[2]/form/div/input[1]')
         code_access.send_keys('260165')# мой код
         #601473 - код Гаврюшина
         code_enter = self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div[2]/form/div/input[2]')
         code_enter.click()
-        #//*[@id="_b_q9y-xew-zr4_join_name"]
-        name_access = self.driver.find_element_by_xpath('//*[@id="_b_vpw-2zy-3k2_join_name"]')
+        
+        name_access = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/form/div/input[4]')
         name_access.send_keys(self.username)
-
+        
         name_enter = self.driver.find_element_by_xpath('/html/body/div[2]/div[1]/div/div[2]/div[2]/form/div/span/button')
         name_enter.click()
     
@@ -54,7 +54,7 @@ class Gavr:
     
     def texting_ones(self):
 
-        sleep(2)
+        sleep(5)
 
         choose_text = self.driver.find_element_by_xpath('//*[@id="message-input"]')
         choose_text.send_keys('Здравствуйте')
@@ -69,7 +69,7 @@ class Gavr:
         choose_text = self.driver.find_element_by_xpath('//*[@id="message-input"]')
         pya.moveTo(choose_text.location['x'] + 446, choose_text.location['y'] + 347)
 
-        for i in range(0, 100):
+        while True:
             choose_text.send_keys('Спам')
             pya.click(button='left')
 
@@ -77,6 +77,6 @@ class Gavr:
 bot = Gavr()
 bot.login()
 bot.header_window()
-#bot.texting_ones()
-bot.spam_attacker()
+bot.texting_ones()
+#bot.spam_attacker()
 
